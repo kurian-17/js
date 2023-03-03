a3b380b177aa8b7b5e29e15d6b4793a984357c7a
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2023-03-03 16:49:57 +0530

    rps

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-let computer = document.querySelector(".computer"),
-player = document.querySelector(".player"),
-results = document.querySelector(".result"),
+const computerText = document.querySelector(".computer"),
+playerText = document.querySelector(".player"),
+resultsText = document.querySelector(".result"),
 buttons = document.querySelectorAll(".opt-btn")
 
-buttons.forEach((button)=> {
-   button.addEventListener("click",item => {
+let player,
+computer,
+result
 
-       player.innerHTML = item.textContent
-       computer.innerHTML = computerChoice()
-   })
+
+buttons.forEach((each)=> {
+   each.addEventListener("click", (button) => {
+       player = button.currentTarget.textContent
+       computerChoice()
+       playerText.textContent = player
+       computerText.textContent = computer
+       
+       resultsText.textContent = showResult()
+    })
 })
 
 function computerChoice(){
-   let randNumber =    Math.floor(Math.random() * 4)
-
+    let randNumber =    Math.floor(Math.random() * 3) + 1
    switch (randNumber) {
     case 1:
-        computer.innerHTML = "Rock"
-        break;
+        computer = "Rock"
+        break
     case 2:
-        computer.innerHTML = "Paper"
-        break;
+        computer = "Paper"
+        break
     case 3:
-        computer.innerHTML = "Scisccor"
+        computer = "Scissor"
+        break
+    default:
         break
    }
+}
+
+
+function showResult() {
+    if(player = computer){
+        return  result = "draw"
+    }
+    else if(player == "Rock"){
+        return (computer == "Paper")? "you lose": "you win"
+    }
+    else if(player == "Paper"){
+        return (computer == "Scissor")? "computer win": "you lose"
+    } 
+    else if(player == "Rock" & computer == "Scissor"){
+        return "You win"
+    }    
+    else if(computer == "Rock" & player == "Scissor"){
+        return "computer win"
+    }
+
 }
