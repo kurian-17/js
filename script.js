bbe19e077faca296062159058961232f4b2d28d5
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2023-03-10 23:42:15 +0530

    cocktail

--- script.js
+++ script.js
@@ -1,57 +1,16 @@
-const computerText = document.querySelector(".computer"),
-playerText = document.querySelector(".player"),
-resultsText = document.querySelector(".result"),
-buttons = document.querySelectorAll(".opt-btn")
+import fetchDrinks from './src/fetchDrinks.js';
+import displayDrink from './src/displaySingleDrink.js';
 
-let player,
-computer,
-result
+const presentDrink = async () => {
+  const id = localStorage.getItem('drink');
+  if (!id) {
+    window.location.replace('index.html');
+  } else {
+    const drink = await fetchDrinks(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    displayDrink(drink);
+  }
+};
 
-
-buttons.forEach((each)=> {
-   each.addEventListener("click", (button) => {
-       player = button.currentTarget.textContent
-       computerChoice()
-       playerText.textContent = player
-       computerText.textContent = computer
-       
-       resultsText.textContent = showResult()
-    })
-})
-
-function computerChoice(){
-    let randNumber =    Math.floor(Math.random() * 3) + 1
-   switch (randNumber) {
-    case 1:
-        computer = "Rock"
-        break
-    case 2:
-        computer = "Paper"
-        break
-    case 3:
-        computer = "Scissor"
-        break
-    default:
-        break
-   }
-}
-
-
-function showResult() {
-    if(player = computer){
-        return  result = "draw"
-    }
-    else if(player == "Rock"){
-        return (computer == "Paper")? "you lose": "you win"
-    }
-    else if(player == "Paper"){
-        return (computer == "Scissor")? "computer win": "you lose"
-    } 
-    else if(player == "Rock" & computer == "Scissor"){
-        return "You win"
-    }    
-    else if(computer == "Rock" & player == "Scissor"){
-        return "computer win"
-    }
-
-}
+window.addEventListener('DOMContentLoaded', presentDrink);
