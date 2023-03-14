0bcfc8b9650a13e74439e37e7e96990afbc6aaf0
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2023-03-15 01:05:42 +0530

    msci url id 403

--- new js/script.py
+++ new js/script.py
@@ -24,4 +24,26 @@ header = {
 resource = requests.get(msci_url, headers=header)
 msci_data = resource.json()
 data = msci_data[0]["url"]
-print(data)
+# print(data)
+
+msci_url_id = f"https://www.msci.com/zh/our-solutions/esg-investing/esg-ratings-climate-search-tool"
+
+company_params = {
+"p_p_id":"esgratingsprofile",
+"p_p_lifecycle":2,
+"p_p_state":"normal",
+"p_p_mode":"view",
+"p_p_resource_id":"showEsgRatingsProfile",
+"p_p_cacheability":"cacheLevelPage",
+"_esgratingsprofile_issuerId": data
+}
+
+msci_response = requests.get(msci_url_id, headers=header, params=company_params)
+
+msci_script = msci_response.content
+print(msci_script)
+
+
+
+
+
